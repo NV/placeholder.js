1a46983b30a9350dfd0d977b58e04846e326afd2
Nikita Vasilyev <me@elv1s.ru>
2010-07-13 11:54:01 +0400

    Allow to submit placeholder value if user actually typed it. Thanks to Avgoustinos(http://github.com/phidias) for bug report!

--- jquery.textPlaceholder.js
+++ jquery.textPlaceholder.js
@@ -13,32 +13,35 @@ jQuery.fn.textPlaceholder = function (color) {
 
 		var default_color = that.style.color;
 		var placeholder = that.getAttribute('placeholder');
+		var input = $(that);
 
 		if (that.value === '' || that.value == placeholder) {
 			that.value = placeholder;
 			that.style.color = color;
+			input.data('placeholder-visible', true);
 		}
-
-		var input = $(that);
 
 		input.focus(function(){
 			this.style.color = default_color;
-			if (this.value == placeholder) {
+			if (input.data('placeholder-visible')) {
+				input.data('placeholder-visible', false);
 				this.value = '';
 			}
 		});
 
 		input.blur(function(){
 			if (this.value === '') {
+				input.data('placeholder-visible', true);
 				this.value = placeholder;
 				this.style.color = color;
 			} else {
 				this.style.color = default_color;
+				input.data('placeholder-visible', false);
 			}
 		});
 
 		that.form && $(that.form).submit(function(){
-			if (that.value == placeholder) {
+			if (input.data('placeholder-visible')) {
 				that.value = '';
 			}
 		});
--- placeholder.js
+++ placeholder.js
@@ -18,28 +18,32 @@ function inputPlaceholder (input, color) {
   if (input.value === '' || input.value == placeholder) {
     input.value = placeholder;
     input.style.color = color;
+    input.setAttribute('data-placeholder-visible', 'true');
   }
 
   var add_event = /*@cc_on'attachEvent'||@*/'addEventListener';
 
   input[add_event](/*@cc_on'on'+@*/'focus', function(){
-    input.style.color = default_color;
-    if (input.value == placeholder) {
-      input.value = '';
-    }
+   input.style.color = default_color;
+   if (input.getAttribute('data-placeholder-visible')) {
+     input.setAttribute('data-placeholder-visible', '');
+     input.value = '';
+   }
   }, false);
 
   input[add_event](/*@cc_on'on'+@*/'blur', function(){
     if (input.value === '') {
+      input.setAttribute('data-placeholder-visible', 'true');
       input.value = placeholder;
       input.style.color = color;
     } else {
       input.style.color = default_color;
+      input.setAttribute('data-placeholder-visible', '');
     }
   }, false);
 
   input.form && input.form[add_event](/*@cc_on'on'+@*/'submit', function(){
-    if (input.value == placeholder) {
+    if (input.getAttribute('data-placeholder-visible')) {
       input.value = '';
     }
   }, false);
