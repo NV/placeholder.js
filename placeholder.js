4c0e59309fa4709db0b9c45db726dd1c592cf359
Nikita Vasilyev <me@elv1s.ru>
2010-07-21 21:02:32 +0400

    Two spaces to tabs

--- placeholder.js
+++ placeholder.js
@@ -6,47 +6,47 @@
  */
 function inputPlaceholder (input, color) {
 
-  if (!input) return null;
+	if (!input) return null;
 
-  // Do nothing if placeholder supported by the browser (Webkit, Firefox 3.7)
-  if (input.placeholder && 'placeholder' in document.createElement(input.tagName)) return input;
+	// Do nothing if placeholder supported by the browser (Webkit, Firefox 3.7)
+	if (input.placeholder && 'placeholder' in document.createElement(input.tagName)) return input;
 
-  color = color || '#AAA';
-  var default_color = input.style.color;
-  var placeholder = input.getAttribute('placeholder');
+	color = color || '#AAA';
+	var default_color = input.style.color;
+	var placeholder = input.getAttribute('placeholder');
 
-  if (input.value === '' || input.value == placeholder) {
-    input.value = placeholder;
-    input.style.color = color;
-    input.setAttribute('data-placeholder-visible', 'true');
-  }
+	if (input.value === '' || input.value == placeholder) {
+		input.value = placeholder;
+		input.style.color = color;
+		input.setAttribute('data-placeholder-visible', 'true');
+	}
 
-  var add_event = /*@cc_on'attachEvent'||@*/'addEventListener';
+	var add_event = /*@cc_on'attachEvent'||@*/'addEventListener';
 
-  input[add_event](/*@cc_on'on'+@*/'focus', function(){
-   input.style.color = default_color;
-   if (input.getAttribute('data-placeholder-visible')) {
-     input.setAttribute('data-placeholder-visible', '');
-     input.value = '';
-   }
-  }, false);
+	input[add_event](/*@cc_on'on'+@*/'focus', function(){
+	 input.style.color = default_color;
+	 if (input.getAttribute('data-placeholder-visible')) {
+		 input.setAttribute('data-placeholder-visible', '');
+		 input.value = '';
+	 }
+	}, false);
 
-  input[add_event](/*@cc_on'on'+@*/'blur', function(){
-    if (input.value === '') {
-      input.setAttribute('data-placeholder-visible', 'true');
-      input.value = placeholder;
-      input.style.color = color;
-    } else {
-      input.style.color = default_color;
-      input.setAttribute('data-placeholder-visible', '');
-    }
-  }, false);
+	input[add_event](/*@cc_on'on'+@*/'blur', function(){
+		if (input.value === '') {
+			input.setAttribute('data-placeholder-visible', 'true');
+			input.value = placeholder;
+			input.style.color = color;
+		} else {
+			input.style.color = default_color;
+			input.setAttribute('data-placeholder-visible', '');
+		}
+	}, false);
 
-  input.form && input.form[add_event](/*@cc_on'on'+@*/'submit', function(){
-    if (input.getAttribute('data-placeholder-visible')) {
-      input.value = '';
-    }
-  }, false);
+	input.form && input.form[add_event](/*@cc_on'on'+@*/'submit', function(){
+		if (input.getAttribute('data-placeholder-visible')) {
+			input.value = '';
+		}
+	}, false);
 
-  return input;
+	return input;
 }
